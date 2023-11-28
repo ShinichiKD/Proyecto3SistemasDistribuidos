8036379e49934f71bba3e5053ef2b163be2cca48
paulquezada <pquezada19@alumnos.utalca.cl>
2023-11-28 18:17:33 -0300

    -

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
